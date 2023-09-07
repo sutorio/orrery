0904bba0aa0508f64c1896aa6c722b9a275320f2
DanCouper <danielcouper81@googlemail.com>
2023-09-07 11:23:02 +0100

    clean slate

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    // Grab dem config values.
-    let config = orrery::parse_app_config();
-    // Initialise the tracing subscriber.
-    orrery::init_tracing();
-    // Set up the database connection pool.
-    let db_conn = orrery::DatabaseConnection::new(&config.database_url).await?;
-    // Start the server.
-    orrery::serve(config, db_conn).await?;
+fn main() -> anyhow::Result<()> {
+    println!("Hello, World!");
 
     Ok(())
 }
